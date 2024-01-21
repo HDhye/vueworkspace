1b7457ae85ef0c922e21118a13c0d89f0c536c2f
Dh H <hdhye89@gmail.com>
2024-01-21 17:53:16 +0900

    router 학습

---
+++ router/src/components/user-profile.vue
@@ -0,0 +1,27 @@
+<template>
+        <div>User :  {{ $route.params.id }}</div>
+</template>
+
+<script>
+export default {
+    mounted() {
+    console.log('User Profile 컴포넌트가 마운트되었습니다:');
+    },
+     computed: {
+    username () {
+      // 곧 `params` 확인할 수 있습니다.
+      return this.$route.params.username
+    }
+  },
+  methods: {
+    goBack () {
+      window.history.length > 1
+        ? this.$router.go(-1)
+        : this.$router.push('/')
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ router/src/routes.vue
@@ -0,0 +1,10 @@
+<script>
+const routes = [
+  { path: '/', redirect: '/home', component: () => import('./components/home-view.vue')},
+    // 동적 라우팅 매칭: 동적 세그먼트는 콜론으로 시작합니다.
+  { path: '/user/:id', component: () => import('./components/user-profile.vue') }, 
+
+]
+
+export default routes
+</script>
